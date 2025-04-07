ecb37695004a944e8e341e2d36e8cda5bb18151a
Sergio R <sergiodrp70@gmail.com>
2025-04-07 10:48:13 -0500

    add morgan

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,16 @@ import cors from 'cors'
 import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
+import morgan from 'morgan'
 
 dotenv.config()
 connectDB()
 
 const app = express()
 app.use(cors(corsConfig))
+
+//logging
+app.use(morgan('dev'))
 
 app.use(express.json()) // leera los valores tipo JSON para probar endpoints con postman o thunderClient 
 
